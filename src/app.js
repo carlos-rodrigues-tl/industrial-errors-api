b38f340f3f431ab36c584f377f73338d9a5c4253
carlos-rodrigues-tl <carlos-rock012@hotmail.com>
2026-04-17 18:54:00 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("./database/db");
+const cors = require("cors");
 
 const errorRoutes = require("./routes/errorRoutes");
 const machineRoutes = require("./routes/machineRoutes");
@@ -7,10 +8,12 @@ const occurrenceRoutes = require("./routes/occurrenceRoutes");
 const searchRoutes = require("./routes/searchRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("src/uploads"));
 app.use("/errors", errorRoutes);
 app.use("/machines", machineRoutes);
@@ -18,15 +21,6 @@ app.use("/occurrences", occurrenceRoutes);
 app.use("/search", searchRoutes);
 app.use("/uploads-api", uploadRoutes);
 app.use("/admin", adminRoutes);
-
-require("dotenv").config();
-const cors = require("cors");
-
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-  }),
-);
 
 // rota teste
 app.get("/", (req, res) => {
